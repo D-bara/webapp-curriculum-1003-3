fcf6e36c86d5485a0af63d6fd7524eed0348b5ec
D-bara <d.bara613677@gmail.com>
2023-01-30 18:40:08 +0900

    ユークリッド互除法を実装

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,3 @@
 object Euclid extends App {
-
-  def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
-  }
-
+  def greatestCommonDivisor(a: Int, b: Int): Int = if(a % b == 0) b else greatestCommonDivisor(b, a%b)
 }
